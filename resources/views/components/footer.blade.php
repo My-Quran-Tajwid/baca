36c41ffb8af28b46106f7f73661ea92cc56ef6ba
iqfareez <60868965+iqfareez@users.noreply.github.com>
2025-01-16 15:16:02 +0800

    :iphone: Responsive footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,11 +1,11 @@
-<footer class="py-12">
-    <div class="container mx-auto flex items-center justify-between text-sm text-gray-500">
+<footer class="py-12 px-4">
+    <div class="container mx-auto flex flex-col sm:flex-row items-center justify-between text-sm text-gray-500">
         <div class="flex items-center space-x-4">
             <a href="https://github.com/My-Quran-Tajwid/" class="hover:underline">GitHub</a>
             <a class="hover:underline">Resources</a>
             <a class="hover:underline">API</a>
         </div>
-        <div>
+        <div class="mt-4 sm:mt-0">
             &copy; 2025 <a href="https://iqfareez.com" class="hover:underline">Muhammad Fareez</a>
         </div>
     </div>
