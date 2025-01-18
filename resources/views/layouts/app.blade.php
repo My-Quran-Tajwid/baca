0167dcc8e91b22b0b5615a14e71c4ea64ccd6ce6
iqfareez <60868965+iqfareez@users.noreply.github.com>
2025-01-18 18:53:35 +0800

    :dizzy: Add Sticky Scrolling to top navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -47,12 +47,29 @@
     <!-- Styles -->
     @yield('styles')
 
-    <script defer src="https://umami.iqfareez.com/script.js" data-website-id="582466c3-2a09-411c-aba1-511bce82fff2"></script>
+    <script defer src="https://umami.iqfareez.com/script.js" data-website-id="582466c3-2a09-411c-aba1-511bce82fff2">
+    </script>
 </head>
 
 <body class="font-sans antialiased">
-    <div class="min-h-screen bg-slate-50 dark:bg-zinc-800">
+    <header x-data="{ isVisible: true, lastScrollY: window.scrollY }" x-init="window.addEventListener('scroll', () => {
+        if (window.scrollY < lastScrollY) {
+            isVisible = true;
+        } else {
+            isVisible = false;
+        }
+        lastScrollY = window.scrollY;
+    })" x-show="isVisible"
+        x-transition:enter="transition ease-out duration-300"
+        x-transition:enter-start="opacity-0 transform -translate-y-full"
+        x-transition:enter-end="opacity-100 transform translate-y-0"
+        x-transition:leave="transition ease-in duration-300"
+        x-transition:leave-start="opacity-100 transform translate-y-0"
+        x-transition:leave-end="opacity-0 transform -translate-y-full" class="fixed top-0 left-0 right-0 z-50">
         @include('layouts.navigation')
+    </header>
+    
+    <div class="min-h-screen bg-slate-50 dark:bg-zinc-800 pt-16">
 
         <!-- Page Heading -->
         @isset($header)
@@ -65,7 +82,7 @@
 
         <!-- Page Content -->
         @yield('content')
-        
+
         <x-footer />
     </div>
 </body>
